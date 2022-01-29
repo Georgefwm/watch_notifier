17031901786d7452e941e2feb592bc87f0caba45
George <49111202+Georgefwm@users.noreply.github.com>
2022-01-29 14:08:42 +1300

    Added comments and changed minor details

--- main.py
+++ main.py
@@ -10,12 +10,19 @@ import sys
 import smtplib
 from datetime import datetime
 
+update_interval = 60 # in seconds
 
+#-----------------------------------------------------------------------------
+# Purpose: Setup a SMTP server so the script can email the link to you
+#
+# @param The post, as a wrapper object (allows for information to be easily pulled
+# @param The model of watch (string), for alert title (no wierd regex to pull model name from title)
+#-----------------------------------------------------------------------------
 def notify_me(post, watch):
     server_ssl = smtplib.SMTP_SSL("smtp.gmail.com", 465)
-    server_ssl.login("[REDACATED]", "[REDACATED]")  # I would recomend making a new dedicated email account for this, also would use the google API login tool
-    sender_email = "[REDACATED]"
-    receiver_email = "[REDACATED]"
+    server_ssl.login("[YOUR_DETAILS]", "[YOUR_DETAILS]")  # I would recomend making a new dedicated email account for this, also would use the google API login tool
+    sender_email = "[YOUR_DETAILS]"
+    receiver_email = "[YOUR_DETAILS]"
     subject = "Alert:  "
     subject += watch
     listing_name = post.title
@@ -37,19 +44,25 @@ def notify_me(post, watch):
     time.sleep(5)
 
 
+#-----------------------------------------------------------------------------
+# Purpose: Connect to the reddit api server and use that connection for the wrapper so it can be easily
+#           interacted with
+#
+# Also logs data for debugging
+#-----------------------------------------------------------------------------
 def create_reddit_instance():
     for i in range(3):
         entry = getpass.getpass()
         in_bytes = entry.encode()
-        hashed = hashlib.sha256(in_bytes)
-        if hashed.hexdigest() == "[REDACATED]":  # hash your email password and insert here
+        hashed = hashlib.sha256(in_bytes) # make sure to hash your password with the algorithm used here
+        if hashed.hexdigest() == "[YOUR_DETAILS]":  # hash your email password and insert here
             print("\nWelcome George\n")
-            reddit = praw.Reddit(client_id = "[REDACATED]",  # enter the details of your API request from reddit
-                                 client_secret = "[REDACATED]",
-                                 user_agent = "[REDACATED]",
-                                 username = "[REDACATED]",
+            reddit = praw.Reddit(client_id = "[YOUR_DETAILS]",  # enter the details of your API request from reddit
+                                 client_secret = "[YOUR_DETAILS]",
+                                 user_agent = "[YOUR_DETAILS]",
+                                 username = "[YOUR_DETAILS]",
                                  password = entry,)
-            entry = "kj34n53k3nk3$!jl2j35'32k5;l23j$)"  # so no password is held in memory afterwards
+            entry = "kj34n53k3nk3$!jl2j35'32k5;l23j$)"  # so no password is held in memory afterwards (theres prob a better way to do this)
             in_bytes = b'kj3423jh423kj48jk2323jhbkfs2'
             try:
                 file_name = "logs.txt"
@@ -77,7 +90,12 @@ def create_reddit_instance():
     print("3 Incorrect attempts, closing...")
     sys.exit()
 
-
+#-----------------------------------------------------------------------------
+# Purpose: Imports models you are looking for from a txt file (allows for quick changing of models to look
+#           for without having to modify the code every time
+#
+# @return array of models in string form
+#-----------------------------------------------------------------------------
 def import_models():
     model_list = []
     print("Reading models from 'models.txt'...\n")
@@ -99,9 +117,11 @@ def import_models():
         file.close()
     return model_list
 
-
+#-----------------------------------------------------------------------------
+# Purpose: Is in charge of setting up and running like a server program (constantly running)
+#-----------------------------------------------------------------------------
 def main():
-    print("please enter password for user: [REDACATED]")
+    print("please enter password for user: [YOUR_DETAILS]")
     reddit = create_reddit_instance()
     MODELS_LIST = import_models()
     script_running = 1
@@ -124,8 +144,10 @@ def main():
         new = r_watchexchange.new(limit=1)
         latest = next(new)
         latest_title = latest.title
-        time.sleep(60)
+        time.sleep(update_interval)
 
-
+#-----------------------------------------------------------------------------
+# Purpose: Good general practice in python
+#-----------------------------------------------------------------------------
 if __name__ == '__main__':
     main()
